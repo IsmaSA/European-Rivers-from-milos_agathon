6c2c641b378f0d667466103e66aa540d9273f1c4
Ismael Soto Almena <isma-sa@hotmail.com>
2023-04-26 23:24:49 +0200

    Update European_rivers.R

--- European_rivers.R
+++ European_rivers.R
@@ -187,3 +187,91 @@ ggsave(
   width = 8.5, height = 7, dpi = 600,
   device = "png", bg = "white", p1
 )
+
+
+
+#############################################################################################################################################
+
+library(rnaturalearth)
+library(sf)
+library(ggplot2)
+
+get_river_map <- function() {
+  
+  # get Europe map
+  europe_map <- ne_countries(scale = "medium", returnclass = "sf") %>%
+    filter(continent == "Europe")
+  
+  # plot the rivers on top of the Europe map
+  p <- ggplot() +
+    geom_sf(data = europe_map, fill = "gray99", color = "gray30") +
+    geom_sf(
+      data = eu_riv_width1,
+      aes(
+        color = factor(ORD_FLOW), size = width,
+        alpha = factor(ORD_FLOW)
+      )
+    ) +
+    
+    geom_sf(data=points_sf,  fill = "purple", alpha=.8, shape = 22, size=0.8)+
+    
+    coord_sf(
+      crs = 4087,
+      xlim = c(bbox["xmin"], bbox["xmax"]),
+      ylim = c(bbox["ymin"], bbox["ymax"])
+    ) +
+    labs(
+      y = "", subtitle = "",
+      x = "",
+      title = "",
+      caption = ""
+    ) +
+    scale_color_manual(
+      name = "",
+      values = c(
+        "#08306b", "#08519c", "#2171b5",
+        "#4292c6", "#6baed6", "#9ecae1",
+        "#c6dbef", "#deebf7"
+      )
+    ) +
+    scale_size(range = c(0, .3)) +
+    scale_alpha_manual(values = c(
+      "3" = 1, "4" = 1, "5" = .7, "6" = .6,
+      "7" = .4, "8" = .3, "9" = .2, "10" = .1
+    )) +
+    theme_minimal() +
+    theme(
+      panel.background = element_blank(),
+      legend.background = element_blank(),
+      legend.position = "none",
+      panel.border = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.grid.major = element_blank(),
+      plot.title = element_text(
+        size = 40, color = "#2171b5", hjust = 0.5, vjust = 0
+      ),
+      plot.subtitle = element_text(
+        size = 14, color = "#ac63a0", hjust = 0.5, vjust = 0
+      ),
+      plot.caption = element_text(
+        size = 10, color = "grey60", hjust = 0.5, vjust = 10
+      ),
+      axis.title.x = element_text(
+        size = 10, color = "grey20", hjust = 0.5, vjust = -6
+      ),
+      legend.text = element_text(
+        size = 9, color = "grey20"
+      ),
+      legend.title = element_text(size = 10, color = "grey20"),
+      strip.text = element_text(size = 12),
+      plot.margin = unit(c(t = 1, r = -2, b = -1, l = -2), "lines"),
+      axis.title.y = element_blank(),
+      axis.ticks = element_blank(),
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank()
+    )
+  
+  return(p)
+}
+
+
